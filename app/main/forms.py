2325b1ac688aa16a3d32311ba3538323d0e09803
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2023-10-16 23:08:17 +0100

    sort field args

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,16 +7,16 @@ from wtforms.validators import InputRequired
 class CookiesForm(FlaskForm):
     functional = RadioField(
         "Do you want to accept functional cookies?",
-        widget=GovRadioInput(),
-        validators=[InputRequired(message="Select yes if you want to accept functional cookies")],
         choices=[("no", "No"), ("yes", "Yes")],
         default="no",
+        validators=[InputRequired(message="Select yes if you want to accept functional cookies")],
+        widget=GovRadioInput(),
     )
     analytics = RadioField(
         "Do you want to accept analytics cookies?",
-        widget=GovRadioInput(),
-        validators=[InputRequired(message="Select yes if you want to accept analytics cookies")],
         choices=[("no", "No"), ("yes", "Yes")],
         default="no",
+        validators=[InputRequired(message="Select yes if you want to accept analytics cookies")],
+        widget=GovRadioInput(),
     )
     save = SubmitField("Save cookie settings", widget=GovSubmitInput())
